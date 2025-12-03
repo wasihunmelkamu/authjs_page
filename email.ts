2002b44f8e49fbaab7db81f6f42b06f31995bc8f
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-03 07:29:42 -0900

    add test demo and try/catch block

--- email.ts
+++ email.ts
@@ -1,4 +1,3 @@
-
 import { Resend } from "resend";
 const resend = new Resend(process.env?.RESEND_API_KEY);
 interface verificationEmailProps {
@@ -9,13 +8,19 @@ export async function sendVerificationEmail({
   email,
   token,
 }: verificationEmailProps) {
-  const verifyLink = `${process.env.NextAuth_URL}/verify-email?token=${token}`;
-  await resend.emails.send({
-    from: "TaskFlow <no-reply@yourdomain.com",
-    to: email,
-    subject: "verify your email",
-    html: `<h2>Verify your email</h2>
-      <p>Click the link below to verify your email address:</p>
-       <a href="${verifyLink}" style="display:inline-block;padding:10px 20px;background:#0070f3;color:white;text-decoration:none;border-radius:5px;"> veify Email</a> <p>This link expires in 1 hour.</p>`,
-  });
+  if (!process.env.NextAuth_URL) throw new Error("NEXTAUTH_UTL not set");
+  const verifyLink = `${process.env.NEXTAUTH_URL}/verifiy_email?token=${token}`;
+  try {
+    const result = await resend.emails.send({
+      from: "TaskFlow <no-reply@resend.dev>",
+      to: email,
+      subject: "verify your email",
+      html: `<h2>Verify your email</h2>
+    <p>Click the link below to verify your email address:</p>
+     <a href="${verifyLink}" style="display:inline-block;padding:10px 20px;background:#0070f3;color:white;text-decoration:none;border-radius:5px;"> veify Email</a> <p>This link expires in 1 hour.</p>`,
+    });
+    console.log("sending", result);
+  } catch (err) {
+    console.error("failed to send verification email ,err", err);
+  }
 }
