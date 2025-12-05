ad341e7d68a07b5e0b4b47a8b9c11f7b93598ec1
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-04 22:09:31 -0900

    make functional of add list

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,24 @@
 // src/app/dashboard/page.tsx
-import { auth,signOut } from "@/auth";
+import { auth, signOut } from "@/auth";
 import { redirect } from "next/navigation";
+import { prisma } from "@/prisma";
 
 export default async function Dashboard() {
   const session = await auth();
   if (!session) redirect("/Login");
-
+  const tasks = await prisma.task.findMany({
+    where: { userId: session?.user?.id },
+    orderBy: { createdAt: "desc" },
+  });
   return (
     <div className="p-8">
       <h1 className="text-2xl font-bold">Welcome, {session.user?.name}!</h1>
       <p className="mt-2 text-gray-600">You are logged in.</p>
-      
+      <div>
+        {tasks.map((task) => (
+          <div key={task.id}>{task.title}</div>
+        ))}
+      </div>
       <ul className="mt-4 space-y-2">
         <li>✅ Task 1</li>
         <li>📝 Task 2</li>
