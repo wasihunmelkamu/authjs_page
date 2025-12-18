a49fef4bc3ccf8a84e239d46b9eb82986b523519
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-18 10:34:43 -0900

    add sentry in to the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,24 @@
 // src/middleware.ts
+import { auth } from "@/auth";
 export { auth as middleware } from "@/auth";
 
+import * as Sentry from "@sentry/nextjs";
+import { NextRequest } from "next/server";
+export async function middlewares(request: NextRequest) {
+  const session = await auth();
+
+  if (session?.user) {
+    Sentry.setUser({
+      id: session.user.id,
+      email: session?.user.email,
+      username: session.user.name || undefined,
+    });
+  }
+  if (session && request.nextUrl.pathname === "/Login") {
+    return Response.redirect(new URL("/dashboard", request.url));
+  }
+}
+
 export const config = {
-  matcher: ["/dashboard/:path*","/dashboard-client/:path*"],
+  matcher: ["/dashboard/:path*", "/dashboard-client/:path*"],
 };
