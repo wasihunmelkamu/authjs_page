e18fb997af1c6b6afd82cccd892b7d2ded9b7e2d
Wasihun Melkamu <melkamuwasihun45@gmail.com>
2025-12-20 00:07:03 -0900

    add user context for sentry

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 // src/app/dashboard/page.tsx
 import { auth } from "@/auth";
 import { prisma } from "@/prisma";
+import * as Sentry from "@sentry/nextjs";
 import { redirect } from "next/navigation";
 
 import TaskListInfinite from "./TaskListInfinite";
@@ -11,6 +12,12 @@ export default async function Dashboard() {
     redirect("/Login");
   }
 
+  // set sentry user context -runs in same serverless function as page
+  Sentry.setUser({
+    id: session.user.id,
+    email: session.user.email as string,
+    username: session.user.name || undefined,
+  });
   const tasks = await prisma.task.findMany({
     where: { userId: session.user?.id },
     orderBy: { createdAt: "desc" },
